4e107ec2199cb8b5c3d727f5a5310c12cbeb268a
pau <paul.e.soukup@gmail.com>
2025-05-25 15:52:52 +0200

    Login Screen

--- app/lib/screens/patient/login/view/login_screen.dart
+++ app/lib/screens/patient/login/view/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:medicall/app/routes.dart';
+import 'package:medicall/contants/colorscheme.dart';
 import 'package:medicall/screens/patient/login/bloc/login_bloc.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -58,52 +59,47 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                   const SizedBox(height: 40),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: 'Enter your email',
-                      prefixIcon: Icon(Icons.email_outlined),
-                      border: OutlineInputBorder(),
-                    ),
-                    keyboardType: TextInputType.emailAddress,
+                  InputField(
+                    hintText: 'Enter your email',
+                    prefixIcon: const Icon(Icons.email_outlined),
+                    onSaved: (value) => _email = value!,
                     validator: (value) =>
                         (value == null || !value.contains('@'))
                             ? 'Enter a valid email'
                             : null,
-                    onSaved: (value) => _email = value!,
                   ),
                   const SizedBox(height: 20),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Enter your password',
-                      prefixIcon: const Icon(Icons.lock_outline),
-                      border: const OutlineInputBorder(),
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _obscurePassword
-                              ? Icons.visibility_off
-                              : Icons.visibility,
-                        ),
-                        onPressed: () {
-                          setState(
-                            () => _obscurePassword = !_obscurePassword,
-                          );
-                        },
+                  InputField(
+                    hintText: 'Enter your password',
+                    prefixIcon: const Icon(Icons.key),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscurePassword
+                            ? Icons.visibility_off
+                            : Icons.visibility,
                       ),
+                      onPressed: () {
+                        setState(
+                          () => _obscurePassword = !_obscurePassword,
+                        );
+                      },
                     ),
-                    obscureText: _obscurePassword,
                     validator: (value) => (value == null || value.length < 6)
                         ? 'Minimum 6 characters'
                         : null,
                     onSaved: (value) => _password = value!,
+                    obscureText: _obscurePassword,
                   ),
                   const SizedBox(height: 10),
                   Align(
                     alignment: Alignment.centerRight,
                     child: TextButton(
                       onPressed: () {},
-                      child: const Text(
+                      child: Text(
                         'Forgot password?',
-                        style: TextStyle(color: Colors.blue),
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
                       ),
                     ),
                   ),
@@ -120,36 +116,52 @@ class _LoginScreenState extends State<LoginScreen> {
                     },
                   ),
                   const SizedBox(height: 16),
-                  const Text(
-                    "Don't have an account? ",
-                    style: TextStyle(fontSize: 14),
-                  ),
                   const SignUpButton(),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 80,
-                        vertical: 14,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                    ),
-                    onPressed: () {
-                      DoctorHomeRoute().go(context);
-                    },
-                    child: const Text(
-                      'Log in as Doctor',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
+                  const LoginAsDoctorButton(),
                 ],
               ),
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class InputField extends StatelessWidget {
+  const InputField({
+    required this.hintText,
+    this.onSaved,
+    this.validator,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.obscureText = false,
+    super.key,
+  });
+  final String hintText;
+  final FormFieldSetter<String>? onSaved;
+  final FormFieldValidator<String>? validator;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+  final bool obscureText;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        fillColor: Theme.of(context).colorScheme.primary.withAlpha(0x2F),
+        filled: true,
+        hintText: hintText,
+        prefixIcon: prefixIcon,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        suffixIcon: suffixIcon,
+      ),
+      keyboardType: TextInputType.emailAddress,
+      validator: validator,
+      onSaved: onSaved,
+      obscureText: obscureText,
     );
   }
 }
@@ -163,8 +175,8 @@ class SubmitButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: () => onSubmit(context),
       style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
         padding: const EdgeInsets.symmetric(
           horizontal: 80,
           vertical: 14,
@@ -178,18 +190,42 @@ class SubmitButton extends StatelessWidget {
   }
 }
 
-class SignUpButton extends StatelessWidget {
-  const SignUpButton({super.key});
+class LoginAsDoctorButton extends StatelessWidget {
+  const LoginAsDoctorButton({super.key});
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
       onPressed: () {
+        DoctorHomeRoute().go(context);
+      },
+      child: Text(
+        'Log in as Doctor',
+        style: TextStyle(
+          color: Theme.of(context).colorScheme.onSecondary,
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpButton extends StatelessWidget {
+  const SignUpButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.all(
+          Theme.of(context).colorScheme.primary.withAlpha(0x2F),
+        ),
+      ),
+      onPressed: () {
         RegisterRoute().push<void>(context);
       },
-      child: const Text(
+      child: Text(
         'Sign up',
-        style: TextStyle(color: Colors.blue),
+        style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
       ),
     );
   }
