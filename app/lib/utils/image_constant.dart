e98d02c7d8b1333f32b0cdb13fc6f69cc4d43217
Jonatan Lindh <jonatan.lindh1@gmail.com>
2025-05-25 15:54:32 +0200

    fix: change imagePath and image constants to const for better performance

--- app/lib/utils/image_constant.dart
+++ app/lib/utils/image_constant.dart
@@ -1,8 +1,8 @@
 class ImageConstant {
   //Image folder path
-  static String imagePath = 'assets/images';
+  static const String imagePath = 'assets/images';
 
   //Common images
-  static String imgClock = '$imagePath/fi-rs-time-fast.png';
-  static String imgMap = '$imagePath/map.png';
+  static const String imgClock = '$imagePath/fi-rs-time-fast.png';
+  static const String imgMap = '$imagePath/map.png';
 }
